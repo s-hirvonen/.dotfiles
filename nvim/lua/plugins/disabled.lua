e76482915b26704136580f7939f4a494780f0e3a
Samuli <hirvonensamuli@gmail.com>
2023-11-29 12:49:58 +0200

    PHP setup

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -2,4 +2,5 @@ return {
   { "saadparwaiz1/cmp_luasnip", enabled = false },
   { "rcarriga/nvim-notify", enabled = false },
   { "echasnovski/mini.indentscope", enabled = false },
+  { "rafamadriz/friendly-snippets", enabled = false },
 }
--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,8 +1,6 @@
 return {
   "nvim-lualine/lualine.nvim",
   opts = function(_, opts)
-    -- remove nvim-navic: LSP "breadcrumb"
-    table.remove(opts.sections.lualine_c, 4)
     -- remove clock
     table.remove(opts.sections.lualine_z, 1)
   end,
