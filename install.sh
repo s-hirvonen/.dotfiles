058370c6909237a3f25e2eba6abbc3a24424731d
Samuli Hirvonen <samuli.hirvonen@bolt.works>
2024-12-04 09:31:19 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -12,33 +12,34 @@ function osx() {
   brew update
   brew tap iveney/mocha # has realpath
   brew install realpath
-	brew install neovim
-	brew install tmux
-	brew install fish
+  brew install neovim
+  brew install tmux
+  brew install fish
   brew install lazygit
   brew install node
   brew install nvm
   brew install jandedobbeleer/oh-my-posh/oh-my-posh
+  brew install fd
+  brew install fzf
+  brew install ripgrep
 
   # Fish
   curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 }
 
 function doIt() {
-	mkdir -p ~/.config
-	mkdir -p ~/.nvm
+  mkdir -p ~/.config
+  mkdir -p ~/.nvm
   touch ~/.secrets
-	for SOURCE in nvim fish
-	do
-		ln -hfis "$(realpath $SOURCE)" ~/.config
-	done
+  for SOURCE in nvim fish; do
+    ln -hfis "$(realpath $SOURCE)" ~/.config
+  done
   ln -hfis "$(realpath tmux/.tmux)" ~
   ln -hfis "$(realpath tmux/.tmux.conf)" ~
-	unset SOURCE
+  unset SOURCE
 }
 
-if [[ "$OSTYPE" == "darwin"* ]]
-then 
+if [[ "$OSTYPE" == "darwin"* ]]; then
   osx
 fi
 
